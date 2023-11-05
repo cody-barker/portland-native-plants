1e06414084836135ee55308e0c2d0d1ab35aebd0
Cody <codybarker.or@gmail.com>
2023-11-05 11:47:53 -0800

    import search provider and wrap the app in App

--- client/src/App.js
+++ client/src/App.js
@@ -9,50 +9,27 @@ import Trees from './pages/Trees'
 import Shrubs from './pages/Shrubs'
 import Grasses from './pages/Grasses'
 import Herbs from './pages/Herbs'
+import { SearchProvider } from './SearchContext'
+
 
 function App() {
 
-  const [allPlants, setAllPlants] = useState([])
-  const [biSearch, setBiSearch] = useState("")
-  const [comSearch, setComSearch] = useState("")
-
-  function handleBiNameSearchState(e) {
-    setBiSearch(e.target.value)
-  }
-
-  function handleComNameSearchState(e) {
-    setComSearch(e.target.value)
-  }
-
-  useEffect(() => {
-    fetch('https://plantlist-database.onrender.com/plants')
-    .then(r => r.json())
-    .then(list => setAllPlants(list))
-  }, [])
-
-  function compare(a, b) {
-    if (a.binomialName.toLowerCase() < b.binomialName.toLowerCase()) {
-        return -1
-    }
-    if (a.binomialName.toLowerCase() > b.binomialName.toLowerCase()) {
-        return 1
-    }
-    return 0
-}
 
   return (
-    <div className="app">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/allplants" element={<AllPlants />}/>
-        <Route path="/trees" element={<Trees />}/>
-        <Route path="/shrubs" element={<Shrubs />}/>
-        <Route path="/grasses" element={<Grasses />}/>
-        <Route path="/herbs" element={<Herbs />}/>
-        <Route path="*" element={<h1>404 not found</h1>}/>
-      </Routes>
-    </div>
+    <SearchProvider>
+      <div className="app">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/allplants" element={<AllPlants />}/>
+          <Route path="/trees" element={<Trees />}/>
+          <Route path="/shrubs" element={<Shrubs />}/>
+          <Route path="/grasses" element={<Grasses />}/>
+          <Route path="/herbs" element={<Herbs />}/>
+          <Route path="*" element={<h1>404 not found</h1>}/>
+        </Routes>
+      </div>
+    </SearchProvider>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { SpeciesProvider } from './SpeciesContext'
-import { SearchProvider } from './SearchContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <SpeciesProvider>
-      <SearchProvider>
         <App />
-      </SearchProvider>
     </SpeciesProvider>
   </BrowserRouter>
 )
