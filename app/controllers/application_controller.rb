dff1d34330523fb97708ba7a3e9106eee48174eb
Cody <codybarker.or@gmail.com>
2023-11-05 10:28:16 -0800

    set up species controller and application controller rescues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_resp
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_resp
+
+  def render_unprocessable_entity_resp(e)
+    render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def render_record_not_found_resp
+    render json: {errors: ["Record not found."]}, status: :not_found
+  end
+
 end
--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,2 +1,32 @@
 class SpeciesController < ApplicationController
+
+  def index
+    species = Species.all
+    render json: species, status: ok
+  end
+
+  def show
+    species = Species.all.find(params[:id])
+    render json: plant, status: ok
+  end
+
+  def create
+    species = Species.create!(species_params)
+    render json: species, status: created
+  end
+
+  private
+    def species_params
+      params.permit(
+        :binomial_name,
+        :common_name,
+        :type,
+        :mig_height,
+        :max_height,
+        :light,
+        :moisture,
+        :photo
+      )
+    end
+
 end
