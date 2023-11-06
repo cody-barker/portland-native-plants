e9c2ef520c7dd174128c7edf6eee784590c75e25
Cody <codybarker.or@gmail.com>
2023-11-06 10:17:48 -0800

    move fallback path to top of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
-  resources :species, only: [:index, :show, :create]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  resources :species, only: [:index, :show, :create]
+
 end
