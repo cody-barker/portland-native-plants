1e824e6dd5f55faa81b4d5ce96c6687c6748b588
Cody <codybarker.or@gmail.com>
2023-11-06 15:22:41 -0800

    update type to speciesType in Plant comp

--- client/src/pages/Plant.js
+++ client/src/pages/Plant.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Plant({plant}) {
 
-    const {binomialName, commonName, type, height, moistureRequirement, lightRequirement} = plant
+    const {binomialName, commonName, speciesType, height, moistureRequirement, lightRequirement} = plant
 
     return(
         <div className="plant-card">
             <h2 id="latin">{binomialName}</h2>
             <h3>{commonName}</h3>
-            <p>Type: {type}</p>
+            <p>Type: {speciesType}</p>
             <p>Height: {height}</p>
             <p>Moisture Requirement: {moistureRequirement}</p>
             <p>Light Requirement: {lightRequirement}</p>
