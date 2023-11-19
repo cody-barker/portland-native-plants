72420af7826b4ac7c1b70a745cea077d1a6fb3c9
Cody <codybarker.or@gmail.com>
2023-11-19 11:23:49 -0800

    remove imports and paths in app for client side type routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,6 @@ import { Routes, Route } from "react-router-dom"
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import AllPlants from './pages/AllPlants'
-import Trees from './pages/Trees'
-import Shrubs from './pages/Shrubs'
-import Grasses from './pages/Grasses'
-import Herbs from './pages/Herbs'
 import { SearchProvider } from './SearchContext'
 import Footer from './components/Footer'
 
@@ -18,10 +14,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/allplants" element={<AllPlants />}/>
-          <Route path="/trees" element={<Trees />}/>
-          <Route path="/shrubs" element={<Shrubs />}/>
-          <Route path="/grasses" element={<Grasses />}/>
-          <Route path="/herbs" element={<Herbs />}/>
           <Route path="*" element={<h1>404 Not Found - Please Return to the Home Page</h1>}/>
         </Routes>
         <Footer />
