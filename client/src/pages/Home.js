79454122a63c68e09001fc52a4c7f3250311ad90
Cody <codybarker.or@gmail.com>
2023-11-18 17:28:06 -0800

    style home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,13 +3,16 @@ import gotter_prairie from '../assets/gotter prairie.jpg'
 
 function Home() {
     return(
-        <header>
-            <h1>Portland Native Plants</h1>
-            <h3>Helping you find the right plant, for the right place.</h3>
-            <p>The Portland Native Plant List is an expansive resource containing plants native to Portland, Oregon.</p>
-            <p>All of the plants on this list could be suitable for backyard habitats and restoration projects alike.</p>
-            <img id="background" src={gotter_prairie} alt="field of camas"></img>
-        </header>
+        <>
+            <header>
+                <h1>Portland Native Plants</h1>
+                <h2>Helping you find the right plant, for the right place.</h2>
+                <h3>An expansive collection of plants native to Portland, Oregon.</h3>
+            </header>
+            <footer>
+                <p>this is a block of text</p>
+            </footer>
+        </>
     )
 }
 
