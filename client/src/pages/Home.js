d0bf8bd161b867a358fb3f87af0d2a4f83f77ad1
Cody <codybarker.or@gmail.com>
2023-11-18 17:01:05 -0800

    style header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,13 +3,13 @@ import gotter_prairie from '../assets/gotter prairie.jpg'
 
 function Home() {
     return(
-        <div>
-            <h1>Welcome to the Portland Native Plant List!</h1>
+        <header>
+            <h1>Portland Native Plants</h1>
             <h3>Helping you find the right plant, for the right place.</h3>
             <p>The Portland Native Plant List is an expansive resource containing plants native to Portland, Oregon.</p>
             <p>All of the plants on this list could be suitable for backyard habitats and restoration projects alike.</p>
             <img id="background" src={gotter_prairie} alt="field of camas"></img>
-        </div>
+        </header>
     )
 }
 
