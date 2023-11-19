9ddb872fe3aba7f6883b36e646849cb57bfbae3b
Cody <codybarker.or@gmail.com>
2023-11-18 18:00:35 -0800

    add github and user icon

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import gotter_prairie from '../assets/gotter prairie.jpg'
+import githubIcon from '../assets/brand-github.svg'
+import userIcon from '../assets/user-icon.svg'
+
 
 function Home() {
     return(
@@ -10,7 +12,8 @@ function Home() {
                 <h3>An expansive collection of plants native to Portland, Oregon.</h3>
             </header>
             <footer>
-                <a target="_blank" href="https://github.com/cody-barker">GitHub</a>
+                <a target="_blank" href="https://github.com/cody-barker/portland-native-plant-list-react-in-progress"><img src={githubIcon}/></a>
+                <a target="_blank" href="https://codybarker.dev"><img src={userIcon}/></a>
             </footer>
         </>
     )
