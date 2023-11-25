cc5a369b62da4542939e3e569bd349ee0c4ef83e
Cody <codybarker.or@gmail.com>
2023-11-25 10:52:01 -0800

    add controlled login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 
 function Login() {
+  const [inputState, setInputState] = useState({
+    username: "",
+    password: "",
+    passwordConfirmation: ""
+  })
 
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const {
+    username,
+    password,
+    passwordConfirmation,
+  } = inputState;
+
+  const onInputChange = (e) => {
+    setInputState({
+      ...inputState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const [isLoading, setIsLoading] = useState(false)
 
   const formData = {
     username,
-    password
+    password,
+    passwordConfirmation
   }
 
   function handleSubmit(e) {
@@ -17,9 +34,16 @@ function Login() {
     fetch("/login")
   }
 
+  //render errors below the form
+
   return(
     <div className="login-container">
+      <p className="dark-green">Login for Admins Only</p>
       <form>
+        <input className="login-input" onChange={onInputChange} name="username" type="text" value={username} placeholder="Username"></input>
+        <input className="login-input" onChange={onInputChange} name="password"type="password" value={password} placeholder="Password"></input>
+        <input className="login-input" onChange={onInputChange} name="passwordConfirmation" type="password" value={passwordConfirmation} placeholder="Confirm Password"></input>
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
