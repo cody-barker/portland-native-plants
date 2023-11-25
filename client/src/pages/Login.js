f157fa68595a5f94a7e21dcc2e2748bcba929869
Cody <codybarker.or@gmail.com>
2023-11-25 11:07:35 -0800

    add handle submit to login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 function Login() {
+  const {setAdmin} = useContext(AdminContext)
   const [inputState, setInputState] = useState({
     username: "",
     password: "",
@@ -21,7 +22,16 @@ function Login() {
   };
 
   const [isLoading, setIsLoading] = useState(false)
-
+  const [errors, setErrors] = useState([])
+  const errorComps = errors.map((e, index) => (
+    <div key={index} className="error-container">
+        {e.errors.map((errorMessage, i) => (
+            <p key={i} className="errors">
+                {errorMessage}
+            </p>
+        ))}
+    </div>
+));
   const formData = {
     username,
     password,
@@ -31,7 +41,21 @@ function Login() {
   function handleSubmit(e) {
     e.preventDefault()
     setIsLoading(true)
-    fetch("/login")
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formData)
+    })
+    .then((r) => {
+      setIsLoading(false)
+      if (r.ok) {
+        r.json().then((admin) => setAdmin(admin))
+      } else {
+        r.json().then((error) => setErrors(error.errors))
+      }
+    })
   }
 
   //render errors below the form
@@ -39,12 +63,13 @@ function Login() {
   return(
     <div className="login-container">
       <p className="dark-green">Login for Admins Only</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input className="login-input" onChange={onInputChange} name="username" type="text" value={username} placeholder="Username"></input>
         <input className="login-input" onChange={onInputChange} name="password"type="password" value={password} placeholder="Password"></input>
         <input className="login-input" onChange={onInputChange} name="passwordConfirmation" type="password" value={passwordConfirmation} placeholder="Confirm Password"></input>
         <button type="submit">{isLoading? "Loading" : "Submit"}</button>
       </form>
+      {errors.length > 0 ? errorComps : null}
     </div>
   )
 }
