b8de84fe822957a5f02683547ac0180e68c9e709
Cody <codybarker.or@gmail.com>
2023-11-25 19:59:39 -0800

    adding speciesType validation but it needs to be formatted to remove attribute prefix

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,3 +1,6 @@
 class Species < ApplicationRecord
+
   validates :commonName, :binomialName, :speciesType, :height, :lightRequirement, :moistureRequirement, presence: true
+  validates :speciesType, inclusion: { in: %w(Tree Shrub Grass Herb),
+    message: "For Type, Please Use: Tree, Shrub, Grass or Herb"}
 end
