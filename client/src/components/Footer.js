e86cd11959bf0db32fb11dfa0a0b46dd1ddeb7a1
Cody <codybarker.or@gmail.com>
2024-01-12 16:00:02 -0800

    restructure footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,16 +4,21 @@ import userIcon from "../assets/user-icon.svg";
 function Footer() {
   return (
     <footer>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://github.com/cody-barker/portland-native-plant-list-react-in-progress"
-      >
-        <img src={githubIcon} alt="link to github repo" />
-      </a>
-      <a target="_blank" rel="noreferrer" href="https://codybarker.dev">
-        <img src={userIcon} alt="developer website" />
-      </a>
+      <div className="container">
+        <div className="footer__content green">Portland Native Plants</div>
+        <div className="footer__links">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/cody-barker/portland-native-plant-list-react-in-progress"
+          >
+            <img src={githubIcon} alt="link to github repo" />
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://codybarker.dev">
+            <img src={userIcon} alt="developer website" />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
