1c93282aec06858202514087fa437e2624af204b
Cody <codybarker.or@gmail.com>
2024-01-15 10:32:21 -0800

    add fern, sedge, and rush to filtered types

--- client/src/pages/AllPlants.js
+++ client/src/pages/AllPlants.js
@@ -72,7 +72,7 @@ function AllPlants() {
     setType(e.target.value);
   }
 
-  const types = ["Tree", "Shrub", "Grass", "Herb"];
+  const types = ["Tree", "Shrub", "Grass", "Herb", "Fern", "Sedge", "Rush"];
 
   function handleHeaderClick(header) {
     setSort({
